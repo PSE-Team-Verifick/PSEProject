bfc9649d47f4e0ec173a6fd7138d26e7fcfbb983
Eli-dd <90352951+Eli-dd@users.noreply.github.com>
2026-01-14 17:57:27 +0100

    introduced skeletons of network_view.py and plot_view.py and enabled switching

--- view/network_view/network_view.py
+++ view/network_view/network_view.py
@@ -1,10 +1,19 @@
 from typing import List
 
+from PySide6.QtWidgets import QVBoxLayout, QLabel
+
 from model.data.network_verification_config import NetworkVerificationConfig
+from view.base_view.insert_view import InsertView
 from view.network_view.network_widget import NetworkWidget
 
-class NetworkView:
+class NetworkView(InsertView):
     pages: List[NetworkWidget]
+
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+        layout.addWidget(QLabel("Network"))
 
     def add_network(self, config: NetworkVerificationConfig):
         pass
--- view/plot_view/plot_view.py
+++ view/plot_view/plot_view.py
@@ -1,2 +1,11 @@
-class PlotView:
-    pass
+from PySide6.QtWidgets import QVBoxLayout, QLabel
+
+from view.base_view.insert_view import InsertView
+
+
+class PlotView(InsertView):
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+        layout.addWidget(QLabel("Plot"))
