5675670faea8bea9c0e65d377d570e5c5e1b0710
Eli-dd <90352951+Eli-dd@users.noreply.github.com>
2026-01-18 16:37:47 +0100

    Cleaned up main.py

--- src/nn_verification_visualisation/main.py
+++ src/nn_verification_visualisation/main.py
@@ -5,18 +5,7 @@ from PySide6.QtWidgets import QApplication
 
 from view.base_view.main_window import MainWindow
 
-
-# Press Strg+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
+def main():
     app = QApplication(sys.argv)
     window = MainWindow()
 
@@ -24,5 +13,5 @@ if __name__ == '__main__':
 
     sys.exit(app.exec())
 
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == '__main__':
+    main()
