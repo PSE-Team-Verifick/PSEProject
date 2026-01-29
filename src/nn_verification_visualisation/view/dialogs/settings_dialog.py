b37fc286a013759ced1abc8f1eeb1ee81c42b152
Eli-dd <90352951+Eli-dd@users.noreply.github.com>
2026-01-29 20:02:36 +0100

    Introduced basic action menu behaviour

--- src/nn_verification_visualisation/view/dialogs/settings_dialog.py
+++ src/nn_verification_visualisation/view/dialogs/settings_dialog.py
@@ -1,7 +1,16 @@
-from typing import List
+from typing import List, Callable
+
+from PySide6.QtWidgets import QWidget, QLabel
 
 from nn_verification_visualisation.view.dialogs.dialog_base import DialogBase
 from nn_verification_visualisation.view.dialogs.settings_option import SettingsOption
 
 class SettingsDialog(DialogBase):
     settings: List[SettingsOption]
+
+    def __init__(self, on_close: Callable[[], None]):
+        super().__init__(on_close, "Settings")
+        print("SettingsDialog init")
+
+    def get_content(self) -> QWidget:
+        return QLabel("Settings")
